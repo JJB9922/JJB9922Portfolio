6a6787275b5063b63935c669a316f43b3cdee58c
Jack <JJB9922@protonmail.com>
2023-11-23 00:48:57 +0000

    fixed timeline

--- JJB9922Portfolio/src/components/VerticalTimeline.tsx
+++ JJB9922Portfolio/src/components/VerticalTimeline.tsx
@@ -1,5 +1,3 @@
-import { Slide } from "react-awesome-reveal";
-
 export default function VerticalTimeline(){
     return(
         <div className="containermx-auto w-full h-full scroll-auto overflow-auto">
@@ -11,15 +9,12 @@ export default function VerticalTimeline(){
             <div className="z-20 flex items-center order-1 bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100 shadow-xl w-8 h-8 rounded-full">
                 <h1 className="mx-auto font-semibold text-lg text-black">1</h1>
             </div>
+            <div className="order-1 bg-gray-500  bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-600 shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] rounded-lg shadow-xl w-5/12 px-6 py-4">
+                <h3 className="mb-3 font-bold text-gray-300 text-xl">Lorem Ipsum</h3>
+                <p className="text-sm leading-snug tracking-wide text-gray-300 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+            </div>
 
-            <Slide>
-                <div className="order-1 bg-gray-500 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-600 shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)] rounded-lg shadow-xl w-5/12 px-6 py-4">
-                    <h3 className="mb-3 font-bold text-gray-300 text-xl">Muay Thaideas</h3>
-                    <p className="text-sm leading-snug tracking-wide text-gray-300 text-opacity-100">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                </div>
-            </Slide>
-               
-                </div>
 
             <div className="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
             <div className="order-1 w-5/12"></div>
