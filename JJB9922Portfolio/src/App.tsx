70f6f39a75ff6340a716d064a27105addcdac554
Jack <JJB9922@protonmail.com>
2023-11-23 23:24:34 +0000

    latest build

--- JJB9922Portfolio/src/App.tsx
+++ JJB9922Portfolio/src/App.tsx
@@ -1,20 +1,26 @@
 import './App.css'
 import DarkModeToggle from './components/ThemeToggle/ThemeToggle';
 import { TypeAnimation } from 'react-type-animation';
+import { Fade, Slide } from 'react-awesome-reveal';
 
 function App() {
 
   return(
     <div className ="light-background dark:dark-background" style={{ overscrollBehavior: 'auto' }}>
+
+      <Slide direction='down'>
       <div className='flex justify-center items-center pb-6 pt-2'>
         <DarkModeToggle />
       </div>
+      </Slide>
+
+      <Fade cascade duration={1000}>
 
       <div className='flex justify-center items-center p-3 text-6xl font-extrabold decoration-4 light-header dark:dark-header'>
           <h1>JJB9922</h1>
       </div>
 
-      <div className='flex justify-center items-center p-6 light-header dark:dark-header'>
+      <div className='flex justify-center items-center p-6 mb-2 light-header dark:dark-header'>
           <TypeAnimation
             sequence={[
               'Junior SWE',
@@ -31,7 +37,7 @@ function App() {
           />
         </div>
 
-        <div className="flex-col pt-3 pb-3 justify-center items-center h-end w-11/12 bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border dark:border-gray-900 border-gray-500 mx-auto shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
+        <div className="flex-col pt-3 pb-6 justify-center items-center h-end w-11/12 bg-gray-900 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-20 border dark:border-gray-900 border-gray-500 mx-auto shadow-[0_2.8px_2.2px_rgba(0,_0,_0,_0.034),_0_6.7px_5.3px_rgba(0,_0,_0,_0.048),_0_12.5px_10px_rgba(0,_0,_0,_0.06),_0_22.3px_17.9px_rgba(0,_0,_0,_0.072),_0_41.8px_33.4px_rgba(0,_0,_0,_0.086),_0_100px_80px_rgba(0,_0,_0,_0.12)]">
           <div className="flex justify-center items-center py-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} className="w-6 h-6 dark:stroke-white stroke-black ">
               <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 6.75L22.5 12l-5.25 5.25m-10.5 0L1.5 12l5.25-5.25m7.5-3l-4.5 16.5" />
@@ -43,7 +49,7 @@ function App() {
               <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 7.5l3 2.25-3 2.25m4.5 0h3m-9 8.25h13.5A2.25 2.25 0 0021 18V6a2.25 2.25 0 00-2.25-2.25H5.25A2.25 2.25 0 003 6v12a2.25 2.25 0 002.25 2.25z" />
             </svg>
 
-            <a href='https://www.github.com/JJB9922' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>Dev Blog</a>
+            <a href='https://dev.to/jjb9922' target='_blank' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>Dev Blog</a>
           </div>
 
           <div className="flex justify-center items-center py-2">
@@ -52,35 +58,35 @@ function App() {
                 <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
               </svg>
 
-              <a href='https://www.github.com/JJB9922' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>United Kingdom</a>
+              <a href='https://en.wikipedia.org/wiki/Bristol' target='_blank' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>United Kingdom</a>
 
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:stroke-white stroke-black ml-12">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:stroke-white stroke-black ml-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
               </svg>
 
-
-              <a href='https://www.github.com/JJB9922' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>Discord</a>
+              <a href='https://discordapp.com/users/1124274817546326066' target='_blank' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>Discord</a>
             </div>
 
-            <div className="flex justify-center items-center py-2">
+            <div className="flex justify-center items-center py-4">
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:stroke-white stroke-black">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M15 10.5a3 3 0 11-6 0 3 3 0 016 0z" />
-                <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
+                <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 3.75V16.5L12 14.25 7.5 16.5V3.75m9 0H18A2.25 2.25 0 0120.25 6v12A2.25 2.25 0 0118 20.25H6A2.25 2.25 0 013.75 18V6A2.25 2.25 0 016 3.75h1.5m9 0h-9" />
               </svg>
 
-              <a href='https://www.github.com/JJB9922' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>United Kingdom</a>
+              <p className='pl-2 text-black dark:text-white'>MSCi: (Experimental) Psychology</p>
 
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:stroke-white stroke-black ml-12">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 01-2.555-.337A5.972 5.972 0 015.41 20.97a5.969 5.969 0 01-.474-.065 4.48 4.48 0 00.978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25z" />
-              </svg>
-
-
-              <a href='https://www.github.com/JJB9922' className='pl-2 underline hover:animate-pulse text-black dark:text-white'>Discord</a>
             </div>
-            
-          </div>
-                
 
+            <div className="flex justify-center items-center py-1.5">
+
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 dark:stroke-white stroke-black">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.436 60.436 0 00-.491 6.347A48.627 48.627 0 0112 20.904a48.627 48.627 0 018.232-4.41 60.46 60.46 0 00-.491-6.347m-15.482 0a50.57 50.57 0 00-2.658-.813A59.905 59.905 0 0112 3.493a59.902 59.902 0 0110.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.697 50.697 0 0112 13.489a50.702 50.702 0 017.74-3.342M6.75 15a.75.75 0 100-1.5.75.75 0 000 1.5zm0 0v-3.675A55.378 55.378 0 0112 8.443m-7.007 11.55A5.981 5.981 0 006.75 15.75v-1.5" />
+              </svg>
+
+              <p className='pl-2 text-black dark:text-white'>University of Bristol</p>
+
+            </div>
+          </div>
+          </Fade>
 
       </div>
   );
